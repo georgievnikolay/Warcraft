594320591179c89aac4a2fb0d0c0549911e7e84a
georgievnikolay <nikolay.georgievfx@gmail.com>
2021-03-28 00:50:28 +0200

    Skeleton

---
+++ build/CMakeFiles/warcraft.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/warcraft.dir/src/main.c.o"
+  "CMakeFiles/warcraft.dir/src/main.c.o.d"
+  "warcraft"
+  "warcraft.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/warcraft.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
